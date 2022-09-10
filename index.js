e314dedd8ceb2c0ea56c37cb67d50cccc7981de3
Shreedhar Chavan <meshree4@gmail.com>
2022-09-10 19:25:58 +0530

    display string logic change

--- index.js
+++ index.js
@@ -36,13 +36,23 @@ const todoList = () => {
     // Format the To-Do list here, and return the output string as per the format given above.
     // FILL YOUR CODE HERE
     let OUTPUT_STRING = "";
-    list.forEach((todoItem) => {
-      OUTPUT_STRING += `[${todoItem.completed ? "x" : " "}] ${todoItem.title} ${
-        todoItem.dueDate === new Date().toISOString().split("T")[0]
-          ? ""
-          : todoItem.dueDate
-      }\n`;
-    });
+
+    for (let i = 0; i < list.length; i++) {
+      if (i === list.length - 1) {
+        OUTPUT_STRING += `[${list[i].completed ? "x" : " "}] ${list[i].title} ${
+          list[i].dueDate === new Date().toISOString().split("T")[0]
+            ? ""
+            : list[i].dueDate
+        }`;
+      } else {
+        OUTPUT_STRING += `[${list[i].completed ? "x" : " "}] ${list[i].title} ${
+          list[i].dueDate === new Date().toISOString().split("T")[0]
+            ? ""
+            : list[i].dueDate
+        }\n`;
+      }
+    }
+
     return OUTPUT_STRING;
   };
 
@@ -82,22 +92,22 @@ todos.add({ title: "Service Vehicle", dueDate: today, completed: false });
 todos.add({ title: "File taxes", dueDate: tomorrow, completed: false });
 todos.add({ title: "Pay electric bill", dueDate: tomorrow, completed: false });
 
-console.log("My Todo-list\n\n");
+console.log("My Todo-list\n");
 
 console.log("Overdue");
 var overdues = todos.overdue();
 var formattedOverdues = todos.toDisplayableList(overdues);
 console.log(formattedOverdues);
-console.log("\n\n");
+console.log("\n");
 
 console.log("Due Today");
 let itemsDueToday = todos.dueToday();
 let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday);
 console.log(formattedItemsDueToday);
-console.log("\n\n");
+console.log("\n");
 
 console.log("Due Later");
 let itemsDueLater = todos.dueLater();
 let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater);
 console.log(formattedItemsDueLater);
-console.log("\n\n");
+console.log("\n");
